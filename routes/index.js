8d3328d0ee9fe68006b178802956b5cfa5def8b7
Gou-Opa <monneymrt1998@gmail.com>
2018-12-18 15:43:11 +0700

    routing to connect Mongo

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var url = 'mongodb://chitbi.ddns.net:27017/ezfood';
+var url = 'mongodb://192.168.1.16:27017/ezfood';
 mongoose.url = url;
 mongoose.connect(url);
 mongoose.Promise = global.Promise;
+var status = "Disconnected";
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
     console.log("mongoose connected");
+    status = "Connected";
 });
 console.log("index/mongohandler said welcome to mongoHandler");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+    res.render('index', { title: 'Express, Mongoose ' + status });
+    
+  
 });
 
 module.exports = router;
